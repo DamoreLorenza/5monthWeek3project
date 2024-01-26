37df2dcd444f6790d1aefabf9036519804c899ca
D'Amore Lorenza <lorenzadamore@outlook.it>
2024-01-26 12:32:45 +0100

    1

---
+++ week3project/src/main/java/lorenza/week3project/entities/Event.java
@@ -0,0 +1,30 @@
+package lorenza.week3project.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "event")
+@Getter
+@Setter
+@ToString
+public class Event {
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+
+    private String title;
+    private String description;
+    private LocalDateTime date;
+    private String location;
+    private int FreeSeats;
+
+}
