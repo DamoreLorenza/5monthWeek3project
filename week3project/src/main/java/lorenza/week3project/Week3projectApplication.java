76a5f48d97cd6cfabc863f661374ed99069dd43d
D'Amore Lorenza <lorenzadamore@outlook.it>
2024-01-26 12:19:30 +0100

    Initial commit

---
+++ week3project/src/main/java/lorenza/week3project/Week3projectApplication.java
@@ -0,0 +1,13 @@
+package lorenza.week3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3projectApplication.class, args);
+	}
+
+}
---
+++ week3project/src/test/java/lorenza/week3project/Week3projectApplicationTests.java
@@ -0,0 +1,13 @@
+package lorenza.week3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
