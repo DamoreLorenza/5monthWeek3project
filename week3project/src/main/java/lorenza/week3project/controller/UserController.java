a2f9e1c4e734b9b99750283c6fa70930f5f9ca53
D'Amore Lorenza <lorenzadamore@outlook.it>
2024-01-26 15:32:52 +0100

    Update UserController.java

--- week3project/src/main/java/lorenza/week3project/controller/UserController.java
+++ week3project/src/main/java/lorenza/week3project/controller/UserController.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
-public class UsersController {
+public class UserController {
     @Autowired
     private UserService userService;
 
