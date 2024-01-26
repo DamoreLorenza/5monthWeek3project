42ea03e3ecd25e0cbbc67f65e937788d2550b247
D'Amore Lorenza <lorenzadamore@outlook.it>
2024-01-26 16:40:30 +0100

    ok

--- week3project/src/main/java/lorenza/week3project/repositories/OrganizerDAO.java
+++ week3project/src/main/java/lorenza/week3project/repositories/OrganizerDAO.java
@@ -1,11 +1,14 @@
 package lorenza.week3project.repositories;
 
 import lorenza.week3project.entities.Organizers;
+import lorenza.week3project.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrganizerDAO extends JpaRepository<Organizers, UUID> {
     List<Organizers> findByOrganizerUUID(UUID organizerUUID);
+    Optional<Organizers> findByEmail(String email);
 }
---
+++ week3project/src/main/java/lorenza/week3project/services/OrganizerService.java
@@ -0,0 +1,45 @@
+package lorenza.week3project.services;
+
+import lorenza.week3project.entities.Event;
+import lorenza.week3project.entities.Organizers;
+import lorenza.week3project.entities.User;
+import lorenza.week3project.exceptions.NotFoundException;
+import lorenza.week3project.payload.NewUserDTO;
+import lorenza.week3project.repositories.EventDAO;
+import lorenza.week3project.repositories.OrganizerDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrganizerService {
+    @Autowired
+    private EventDAO eventDAO;
+
+    @Autowired
+    private OrganizerDAO organizerDAO;
+
+    public Organizers findByEmail(String email) throws NotFoundException {
+        return organizerDAO.findByEmail(email).orElseThrow(() -> new NotFoundException("organizzatore con email " + email + " non trovato"));
+    }
+
+    public Organizers findByUUID(UUID uuid) {
+        return organizerDAO.findById(uuid).orElseThrow(() -> new NotFoundException(uuid));
+    }
+
+    public Organizers findByUUIDAndUpdate(UUID uuid, Organizers body) {
+        Organizers found = this.findByUUID(uuid);
+        found.setOrganizer(body.getOrganizer());
+        found.setPassword(body.getPassword());
+        return organizerDAO.save(found);
+    }
+
+    public void findByUUIDAndDelete(UUID uuid) {
+        Organizers found = this.findByUUID(uuid);
+        organizerDAO.delete(found);
+    }
+    
+
+    }
